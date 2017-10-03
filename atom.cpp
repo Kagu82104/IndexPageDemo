9b3fb364d0d17e4f8879ece96a9a00f0ef882ea8
Kagu82104 <vincent82104@gmail.com>
2017-10-04 00:02:31 +0800

    no message

---
+++ atom.cpp
@@ -0,0 +1,21 @@
+#include "number.h"
+#include "atom.h"
+#include "variable.h"
+#include <string>
+using std::string;
+
+Atom :: Atom (string s):_symbol(s) {}
+	string Atom :: symbol(){return _symbol;}
+	bool Atom :: match(Number num){return false;};
+	bool Atom :: match(Variable &var){
+  	bool ret = var.getassignable();
+    if(ret || var.value()==""){
+      var.setvalue(_symbol) ;
+      var.setassignable(false);
+			return true;
+    }
+		else if(_symbol==var.symbol()){
+			return true;
+		}
+    return ret;
+  }
