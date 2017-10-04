6775035b8f40a67909299ce5438b97e9fec56315
Kagu82104 <vincent82104@gmail.com>
2017-10-04 11:46:57 +0800

    no message

--- number.cpp
+++ number.cpp
@@ -9,7 +9,7 @@ Number :: Number(int v):_value(v),_symbol(std::to_string(v)){}
   string Number :: symbol(){return _symbol;}
   bool Number :: match(Number num){return _value==num._value;}
   bool Number :: match(Atom at){return _symbol==at.symbol();}
-  bool Number :: match(Variable &var){
+  bool Number :: match(Variable var){
   	bool ret = var.getassignable();
   	if(ret ||(var.value()==std::to_string(_value))){
   		return true;
