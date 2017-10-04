0874f516e13ace69a8bc2febc0d8f4a499049bc9
Kagu82104 <vincent82104@gmail.com>
2017-10-04 11:36:54 +0800

    no message

--- number.cpp
+++ number.cpp
@@ -5,7 +5,7 @@
 
 using std::string;
 Number :: Number(int v):_value(v),_symbol(std::to_string(v)){}
-  int Number :: value(){return _value;}
+  int Number :: value(){return std::to_string(_value);}
   string Number :: symbol(){return _symbol;}
   bool Number :: match(Number num){return _value==num._value;}
   bool Number :: match(Atom at){return _symbol==at.symbol();}
